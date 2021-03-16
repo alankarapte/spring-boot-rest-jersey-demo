7d92f2ec5af2cc16e7fe470b38592da2827a848f
alankarapte <alankar@vedirasolutions.com>
2021-03-17 02:25:14 +0530

    seeded some employee records

--- src/main/java/com/alankar/springbootrestjerseydemo/SpringBootRestJerseyDemoApplication.java
+++ src/main/java/com/alankar/springbootrestjerseydemo/SpringBootRestJerseyDemoApplication.java
@@ -1,10 +1,34 @@
 package com.alankar.springbootrestjerseydemo;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alankar.springbootrestjerseydemo.dao.EmployeeDAO;
+import com.alankar.springbootrestjerseydemo.model.Employee;
+
 @SpringBootApplication
 public class SpringBootRestJerseyDemoApplication {
+
+	@Autowired
+	private EmployeeDAO employeeDAO;
+
+	@PostConstruct
+	public void initDb() {
+		employeeDAO.saveAll(Stream.of( 
+				new Employee(1, "Alankar", "DEV"),
+				new Employee(2, "Sneha", "HR"),
+				new Employee(3, "Venky", "DEV"),
+				new Employee(4, "Rajesh", "DEV"),
+				new Employee(5, "Dnyanada", "QA"))
+				.collect(Collectors.toList())
+				);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestJerseyDemoApplication.class, args);
