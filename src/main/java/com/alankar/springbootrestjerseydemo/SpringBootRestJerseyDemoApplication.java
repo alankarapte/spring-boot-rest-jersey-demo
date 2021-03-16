acbddaf15ff16d3bffcbea01e58f6f9160929a7a
alankarapte <alankar@vedirasolutions.com>
2021-03-16 19:15:47 +0530

    initial commit

---
+++ src/main/java/com/alankar/springbootrestjerseydemo/SpringBootRestJerseyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alankar.springbootrestjerseydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJerseyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestJerseyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alankar/springbootrestjerseydemo/SpringBootRestJerseyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alankar.springbootrestjerseydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestJerseyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
